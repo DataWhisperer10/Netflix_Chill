34a75a7854ba71bf3084df974617b213fd964907
Gajanand K <gajanandkkumawat95@gmail.com>
2024-04-18 22:15:38 +0530

    added the splash screen animation using the Lottie Package. Also created the Bottom Navigation Bar

---
+++ lib/screens/hot&new_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HotNewScreen extends StatefulWidget {
+  const HotNewScreen({super.key});
+
+  @override
+  State<HotNewScreen> createState() => _HotNewScreenState();
+}
+
+class _HotNewScreenState extends State<HotNewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/widget/bottom_nav_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/screens/home_screen.dart';
+import 'package:netflix/screens/hot&new_screen.dart';
+import 'package:netflix/screens/search_screen.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            bottomNavigationBar: Container(
+              color: Colors.black,
+              height: 80,
+              child: const TabBar(
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.home_filled),
+                    text: "Home",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.search),
+                    text: "Search",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.photo_library_outlined),
+                    text: "Hot & New",
+                  ),
+                ],
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.white60,
+              ),
+            ),
+            body: const TabBarView(
+                children: [HomeScreen(), SearchScreen(), HotNewScreen()])));
+  }
+}
