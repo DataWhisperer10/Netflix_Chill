d5306b3057a99800d920c4e0dacddd3ed621f3f6
Gajanand K <gajanandkkumawat95@gmail.com>
2024-04-20 15:27:03 +0530

    setup the searchbar

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 import 'package:netflix/common/utils.dart';
+import 'package:netflix/models/search_model.dart';
 import 'package:netflix/models/tv_series_model.dart';
 import 'package:netflix/models/upcoming_movie_model.dart';
 
@@ -46,5 +47,20 @@ class ApiServices {
       return TvSeriesModel.fromJson(jsonDecode(response.body));
     }
     throw Exception("Failed to load Top Rated TV Series");
+  }
+
+  Future<SearchModel> getSearchMovie(String searchText) async {
+    endPoint = "search/movie?query=$searchText";
+    final url = "$baseUrl$endPoint$key";
+    print("Search Url is $url");
+    final response = await http.get(Uri.parse(url), headers: {
+      'Authorization':
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTIzMjBhMTg2MDEyNGI2NTMyNTUwNWNmZmMxMzhkNSIsInN1YiI6IjY1ZjkwYjNkMDdlMjgxMDE4NmMxNzc1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EjMbh-eTFChZKOFEl1emOXQobNYF-xJkP0jsSApZCac"
+    });
+    if (response.statusCode == 200) {
+      log("Success response: ${response.body}");
+      return SearchModel.fromJson(jsonDecode(response.body));
+    }
+    throw Exception("Failed to load Searched Movies");
   }
 }
